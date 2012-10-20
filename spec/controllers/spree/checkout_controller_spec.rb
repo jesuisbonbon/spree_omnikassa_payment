541b0e652af8225dd1671bfbb15681c373737969
Willian <willian@pythonheads.nl>
2012-10-20 15:31:07 +0200

    Using Spree::LegacyUser instead of Spree:User in test

--- spec/controllers/spree/checkout_controller_spec.rb
+++ spec/controllers/spree/checkout_controller_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe Spree::CheckoutController do
   before(:each) do
-    @pm = Spree::PaymentMethod::Omnikassa.new(:name => "Omnikassa", :environment => "test")
+    @pm = Spree::PaymentMethod::Omnikassa.new(:name => "Omnikassa", 
+                                              :environment => "test")
     @pm.save
     @pm.preferred_merchant_id = @merchant_id
     @pm.preferred_key_version = @key_version
     @pm.preferred_secret_key = @secret_key
     @routes = Spree::Core::Engine.routes  # render_views
   end
+
   let(:order) { mock_model(Spree::Order,
                            :checkout_allowed? => true,
                            :completed? => false,
@@ -19,7 +21,9 @@ describe Spree::CheckoutController do
                            :state => "payment",
                            :total => BigDecimal("123.45"),
                            :payments => []).as_null_object }
-  before { controller.stub :current_order => order, :current_user => Spree::User.anonymous! }
+  
+  before { controller.stub :current_order => order, 
+                           :current_user => Spree::LegacyUser.anonymous! }
 
   it 'should show omnikassa form on payment' do
     get :edit, {:state => 'payment'}
